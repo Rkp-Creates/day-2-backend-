180590e3b8100bfb6f4215ef28fa46dc4969f7cc
Rkp-Creates <codes.by.rahul@gmail.com>
2026-03-16 21:35:24 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()  //server instance create krna
+
+app.get('/', (req, res) =>{
+    res.send("Hello world")
+})
+
+// app.get("/about", function(req,res){
+//     res.send("this is about page")
+// })
+app.get("/home", (req,res)=>{
+    res.send("this is  home page")
+})
+
+app.listen(3000)   //server start krna
